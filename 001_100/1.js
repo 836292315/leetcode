a801b1b07a9cb9cb7d7cc6c4b42765aa342743e9
836292315 <836292315@qq.com>
2018-01-24 09:43:10 +0800

    1

---
+++ 001_100/1.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    var house ={};
+    for(var i=0;i<nums.length;i++){
+        var val1 = nums[i];
+        var val2 = target -nums[i];
+        if(val2 in house){
+            return [house[val2],i];
+        }
+        house[val1] = i;
+    }
+    return null;
+};
