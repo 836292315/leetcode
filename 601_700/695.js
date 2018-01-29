667280fdbec991408ce800762a85f0bfd90535e3
836292315 <836292315@qq.com>
2018-01-29 09:55:26 +0800

    max area of Island

---
+++ 601_700/695.js
@@ -0,0 +1,18 @@
+var maxAreaOfIsland = function(grid) {
+    var h = grid.length,w = grid[0].length,result = 0;
+    function dfs(i,j){
+        if( i>=0 && i<h && j>=0 && j<w){
+            if(grid[i][j]===1){
+                grid[i][j] =0;
+               return 1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1);
+            }
+        }
+        return 0;
+    }
+    for(var x =0;x<h;x++){
+        for(var y =0;y<w;y++){
+            result = Math.max(result,dfs(x,y));
+        }
+    }
+    return result;
+};
