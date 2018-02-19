1d6f29b76e738fad81c516afac12fb48f32ffec4
836292315 <836292315@qq.com>
2018-02-19 08:53:15 +0800

    Fizz Buzz

---
+++ 401_500/412.js
@@ -0,0 +1,14 @@
+var fizzBuzz = function(n) {
+     var result = [],
+         str,
+         i = 1;
+    while( i <= n ){
+        str = "";
+        if( i%3===0 ) str = 'Fizz';
+        if( i%5===0 ) str += 'Buzz';
+        if(!str) str += i;
+        result.push( str );
+        i++;
+    }
+    return result;
+};
