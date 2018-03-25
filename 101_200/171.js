ed30ac4a27d845f35fd0708376f3db2f40528036
836292315 <836292315@qq.com>
2018-03-25 20:55:14 +0800

    excel sheet column number

---
+++ 101_200/171.js
@@ -0,0 +1,7 @@
+var titleToNumber = function(s) {
+    var result = 0;
+    for(var i = 0; i < s.length; i++ ){
+        result = result*26 + (s.charCodeAt(i) - 65+1);
+    }
+    return result;
+};
