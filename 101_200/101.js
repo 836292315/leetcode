454f4f8dfa1d6cfe7ae4636c85029e127beebe3b
836292315 <836292315@qq.com>
2018-05-13 09:39:49 +0800

    symmetric tree

---
+++ 101_200/101.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) {
+        return false;
+    }
+    return helper(root.left, root.right);
+
+    function helper(left, right) {
+        if (left === null && right === null) {
+            return true;
+        }
+        if (left === null || right === null) {
+            return false;
+        }
+        if (left.val === right.val) {
+            return helper(left.left, right.right) && helper(left.right, right.left);
+        } else {
+            return false;
+        }
+    }
+};
