f12f85e94b50fd63b04234eb877fdb297db1a5cf
836292315 <836292315@qq.com>
2018-05-22 10:52:01 +0800

    longest univalue path

---
+++ 601_700/687.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestUnivaluePath = function(root) {
+    if (!root) {
+        return 0;
+    }
+    return Math.max(
+        longestUnivaluePath(root.left),
+        longestUnivaluePath(root.right),
+        helper(root.left, root.val) + helper(root.right, root.val));
+
+    function helper(node, val) {
+        if (!node || node.val !== val) {
+            return 0;
+        }
+        return Math.max(helper(node.left, val), helper(node.right, val)) + 1;
+    }
+}
