ed7acd57ebc3da22e3264c11b2a97c3f95e37f6b
836292315 <836292315@qq.com>
2018-05-27 09:50:26 +0800

    linked list cycle

---
+++ 101_200/141.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head) {
+        return false;
+    }
+    let slow = head,
+        fast = head.next;
+    while (slow && fast) {
+        if (slow === fast) {
+            return true;
+        }
+        slow = slow.next;
+        if (fast.next) {
+            fast = fast.next.next;
+        } else {
+            return false;
+        }
+    }
+    return false;
+};
+
+let slow = head;
+let fast = head;
+
+while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+        return true;
+    }
+}
+
+return false;
