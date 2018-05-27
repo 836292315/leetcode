ffadfeee1ff77a90745e0bba0c8551ea14fd699e
836292315 <836292315@qq.com>
2018-05-27 09:52:47 +0800

    linked list Cycle

--- 101_200/141.js
+++ 101_200/141.js
@@ -29,17 +29,3 @@ var hasCycle = function(head) {
     }
     return false;
 };
-
-let slow = head;
-let fast = head;
-
-while (fast && fast.next) {
-    fast = fast.next.next;
-    slow = slow.next;
-
-    if (fast === slow) {
-        return true;
-    }
-}
-
-return false;
