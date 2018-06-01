be2fb00ad5ec2888418bdc78e197ca3ffb218a6e
836292315 <836292315@qq.com>
2018-06-01 15:36:34 +0800

    implement stack using queues

---
+++ 201_300/225.js
@@ -0,0 +1,53 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function() {
+    this.stack = [];
+};
+
+/**
+ * Push element x onto stack. 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    let len = this.stack.length;
+    for (let i = 1; i < len; i++) {
+        let tem = this.stack.shift();
+        this.stack.push(tem);
+    }
+    return this.stack.shift();
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.stack.length === 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = Object.create(MyStack).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
