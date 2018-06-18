5591c3516aa370abb1518fa25be7be2db4c66763
836292315 <836292315@qq.com>
2018-06-18 10:10:46 +0800

    remove linked list elements

---
+++ 201_300/203.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    /*
+    if (head === null) {
+         return null;
+     }
+     head.next = removeElements(head.next, val);
+     return head.val === val ? head.next : head;
+     */
+
+    if (head === null) {
+        return head;
+    }
+    head.next = removeElements(head.next, val);
+    if (head.val === val) {
+        return head.next;
+    } else {
+        return head;
+    }
+};
