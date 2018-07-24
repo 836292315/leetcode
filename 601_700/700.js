0bdfc21766c8e7322d667317a3d4c187b598c5d7
836292315 <836292315@qq.com>
2018-07-24 09:22:49 +0800

    search in a binary search tree

---
+++ 601_700/700.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if (root.val === val) {
+        return root;
+    } else if (val < root.val && root.left !== null) {
+        return searchBST(root.left, val);
+    } else if (val > root.val && root.right !== null) {
+        return searchBST(root.right, val);
+    } else return [];
+};
