816bfbcc752f91f418cbeac3703786fdf87900f7
836292315 <836292315@qq.com>
2018-08-04 10:09:54 +0800

    max chunks to make sorted

---
+++ 701_800/769.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+    let res = 0,
+        max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        max = Math.max(max, arr[i]);
+        if (max === i) {
+            res++;
+        }
+    }
+    return res;
+};
