0e602a179e75fb0a7f0a350ff3b56950748f5277
836292315 <836292315@qq.com>
2018-08-12 10:16:35 +0800

    combination sum

---
+++ 001_100/39.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let result = [];
+    helper(result, candidates, [], target, 0);
+    return result;
+
+    function helper(result, candidates, tem, target, start) {
+        if (target > 0) {
+            for (let i = 0; i < candidates.length; i++) {
+                tem.push(candidates[i]);
+                helper(result, candidates, tem, target - candidates[i], i);
+                tem.pop();
+            }
+        }
+        if (target === 0) {
+            result.push(tem);
+        }
+    }
+};
