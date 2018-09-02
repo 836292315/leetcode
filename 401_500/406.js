e2a67ba8fc68df6b79003bd0f25cc3aebbcc4ddd
836292315 <836292315@qq.com>
2018-09-02 11:22:04 +0800

    queue reconstruction by height

---
+++ 401_500/406.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function(people) {
+    people.sort(helper);
+    let res = [];
+    for (let i = 0; i < people.length; i++) {
+        res.splice(people[i][1], 0, people[i]);
+    }
+    return res;
+
+    function helper(a, b) {
+        if (a[0] !== b[0]) {
+            return b[0] - a[0];
+        } else {
+            return a[1] - b[1];
+        }
+    }
+};
