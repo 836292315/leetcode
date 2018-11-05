1a832c0df9509d6799677b1b8cc33d11d001cc83
fei <836292315@qq.com>
2018-11-05 09:00:19 +0800

    odd even linked list

---
+++ 301_400/328.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+    if (head) {
+        let odd = head;
+        let even = head.next;
+        let evenHead = even;
+        while (even && even.next) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+    }
+    return head;
+};
