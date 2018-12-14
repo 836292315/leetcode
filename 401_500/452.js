b37bab6a7b4a319663b6bc2559f9f3655c25acc9
Fahee <836292315@qq.com>
2018-12-14 09:51:08 +0800

    Minimum Number of Arrows to Burst Balloons

---
+++ 401_500/452.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    if(points.length === 0) return 0;
+    points.sort((a, b) => a[1] - b[1]);
+    let res = 1;
+    let pos = points[0][1];
+    for (let i = 1; i < points.length; i++) {
+        if (pos >= points[i][0]) continue;
+        res++;
+        pos = points[i][1];
+    }
+    return res;
+};
