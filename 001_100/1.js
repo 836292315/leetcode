17560022602af913315f66199cda2fe537a8aa92
Fahee <836292315@qq.com>
2018-12-19 09:43:24 +0800

    Two Sum

--- 001_100/1.js
+++ 001_100/1.js
@@ -1,10 +1,10 @@
-var twoSum = function(nums, target) {
-    var house ={};
-    for(var i=0;i<nums.length;i++){
+var twoSum = function (nums, target) {
+    var house = {};
+    for (var i = 0; i < nums.length; i++) {
         var val1 = nums[i];
-        var val2 = target -nums[i];
-        if(val2 in house){
-            return [house[val2],i];
+        var val2 = target - nums[i];
+        if (val2 in house) {
+            return [house[val2], i];
         }
         house[val1] = i;
     }
